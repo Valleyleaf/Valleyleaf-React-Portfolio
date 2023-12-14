8356aa16422301dfd0a7bd345ed3d651e8b7889e
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-12-13 23:40:31 -0400

    Added default contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,69 @@
 import React from 'react';
 import { useState } from "react";
 
-export default function Contact(){
-    return (
-      <div>
-        <div className='flex-container center-content'>
-          <form action="input"></form>
-          <p>Contact</p>
-        </div>
+// export default function Contact(){
+//     return (
+//       <div>
+//         <div className='flex-container center-content'>
+//           <form action="input"></form>
+//           <p>Contact</p>
+//         </div>
+//       </div>
+//     );
+//   }
+
+const Contact = () => {
+  // State to manage form input values
+  const [formInput, setformInput] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setformInput((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <div className='flex-container center-content flex-container-columns'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name: 
+            <input
+              type='text'
+              name='name'
+              value={formInput.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Email: 
+            <input
+              type='email'
+              name='email'
+              value={formInput.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Message: 
+            <textarea
+              name='message'
+              value={formInput.message}
+              onChange={handleChange}
+            />
+          </label>
+          <button type='submit'>Submit</button>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+export default Contact;
