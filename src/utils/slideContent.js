da1914fff5d357a772cff2ed503d0a2a87e816b0
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-12-22 01:02:33 -0400

    Finalizing

--- src/utils/slideContent.js
+++ src/utils/slideContent.js
@@ -10,7 +10,7 @@ let slideImage = [
 let slideTitle = [
     'moodMusic.com',
     'Horizon Health',
-    'Battle of Dominance',
+    'Javascript Password Generator',
     'FOUR CATERING',
     'Note Taker',
     'SVG Logo Maker',
@@ -18,7 +18,7 @@ let slideTitle = [
 
 let slideContent = [
     'Worked with a team of 4 to deploy moodMusic.com. A browser based application that allows you to generate a spotify playlist based on your location. This application runs off two seperate API\'s and was built using HTML, CSS and JavaScript.',
-    'I made this project to better understand what to expect in the future of the industry that I wish to enter. By creating this project I was able to learn how to do basic debugging, formating proper semantic HTLM, optimize CSS classes and also how to manage my repository.',
+    'Compiled a Javascript Password Generator.',
     'A 15 year endevor. Modded gamemode for Warcraft 3 running over 1000 custom scripts, 3 seperate gamemodes, 50+ custom characters and hundreds of custom spells + items. Tools used include WC3GUI & JASS. A show of dedication more than anything else.',
     'Worked with a team of 4 in deployed a catering storefront using Heroku. Application supported on both front and back-end',
     'Deployed a Note-taker app that uses local storage to allow the user to host notes in their browser cache.',
@@ -29,7 +29,7 @@ let slideContent = [
 let slideLink = [
     'https://magicianjoshua.github.io/weatherMusic/',
     'https://valleyleaf.github.io/02-CHALLENGE/',
-    'https://www.hiveworkshop.com/threads/battle-of-dominance-v2-0-arcane-showdown.326630/',
+    'https://valleyleaf.github.io/Javascript-Password_generator/',
     'https://four-catering-cb97eed1384a.herokuapp.com/',
     'https://valleyleaf.github.io/Note-Taker/',
     'https://drive.google.com/file/d/16zmWc7v_zhW2jK7pUx5bjCC0sbdZwmL4/view',
@@ -40,7 +40,7 @@ let slideLink = [
 let slideRepo = [
     'https://github.com/MagicianJoshua/weatherMusic',
     'https://github.com/Valleyleaf/02-CHALLENGE',
-    'https://www.hiveworkshop.com/threads/battle-of-dominance-v2-0-arcane-showdown.326630/',
+    'https://github.com/Valleyleaf/Javascript-Password_generator',
     'https://github.com/Valleyleaf/Catering-Application',
     'https://github.com/Valleyleaf/Note-Taker',
     'https://github.com/Valleyleaf/SVGLogoMaker',
