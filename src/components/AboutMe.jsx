28c53802328c3a3d27e5c2beb0ec7b6eb6079a3f
Aston Dahllöf <Aston_dahllof@outlook.com>
2024-01-03 10:54:56 -0400

    Updating about me. Got build function working but navbar is still broken for some reason

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,9 +8,9 @@ export default function About(){
           <div className='flex-container  flex-container-columns center-content'>
           <h2>About me</h2>
           <div className='noWrap'>
-          <p>Hey there! Welcome to my corner of the internet. I'm a versatile professional juggling a mix of tech and creativity. Started out in IT sales and people management, and now I'm diving into the coding world through a bootcamp as it has always been a latient passion .</p>
-          <p>I dabble in multimedia creation – think photos, videos, and sounds. Don't hire me for sound editing, I am completely tone-deaf despite all my efforts to complete my editing trifecta.  When I'm not in front of my monitor, you'll find me lost in the world of art, painting, and creating things just for the fun of it.</p>
-          <p>Join me as I navigate the wild mix of creativity and code. It's a bit of a crazy ride, but it's my way of shaping the future with a touch of innovation and a whole lot of artistic expression.</p>
+          <p>Howdy. My name is Aston .A Dahllof. I'm a versatile professional juggling a mix of tech and creativity. Started out in IT sales and people management, and now I'm diving into the coding world through a bootcamp as it has always been a latient passion .</p>
+          <p>I dabble in multimedia creation. Photos, videos, and sound. When I'm not in front of my monitor, you'll find me lost in the world of art, painting, and creating things just for the fun of it.</p>
+          <p>Join me as I navigate the wild mix of creativity and code.</p>
           </div>
           </div>
           <img className='profile-img' src={profilePicture} alt="ProfileImage"/>
