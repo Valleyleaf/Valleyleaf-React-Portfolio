73817a11378d74f5f63392f1e194db42127bd93a
Aston .A Dahllof <137734906+Valleyleaf@users.noreply.github.com>
2024-03-18 16:31:38 -0300

    Updating Header to include link to home. Fixing mobile CSS for specific devices. Fixing ContactForm not giving me phone number

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import {Link, useLocation} from 'react-router-dom';
 import linkedinButton from '../../assets/img/linkedin-fill-svgrepo-com.svg'
 import twitterButton from '../../assets/img/twitter-fill-svgrepo-com.svg'
 import gitHubButton from '../../assets/img/github-fill-svgrepo-com.svg'
 import './header.css'
 
 export default function Header(){
+  const currentPage = useLocation().pathname;
+
+  const handleLinkClick = (event) => {
+    document.querySelectorAll('nav').forEach(link => {
+      link.className.remove('nav-active');
+    });
+    event.target.className.add('nav-active');
+  };
     return (
       <div>
         <div className='Header-Top flex-container-row mainColor'>
-          <div className='header-Text'>
-            <h2>Aston Andersson Dahllöf</h2>
-          </div>
+          
+        <Link
+          to="/"
+          className='header-Text titleAnimation'
+          onClick={handleLinkClick}
+        >
+          <h2>Aston Andersson Dahllof</h2>
+        </Link>
 
           <div className='keepRight center-content'>
           <a href="https://www.linkedin.com/in/aston-andersson-dahllof-b4310ab5/" target="_blank">
