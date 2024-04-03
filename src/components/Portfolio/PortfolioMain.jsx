eedc1dba549a1dfcf7e804b12cc72c2cb655bf36
Aston .A Dahllof <137734906+Valleyleaf@users.noreply.github.com>
2024-04-03 20:08:41 -0300

    Got Portfolio windows to work properly. Clean up CSS on isle 4

--- src/components/Portfolio/PortfolioMain.jsx
+++ src/components/Portfolio/PortfolioMain.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {projectTitle, projectText, projectImage, projectRepoLink, projectDeployLink} from '../../utils/projectContent'
 import './portfolio.css'
+import deployIcon from '../../assets/img/link-alt-1-svgrepo-com.svg';
+import repoIcon from '../../assets/img/inbox-alt-1-svgrepo-com.svg';
 
 function PortfolioMain() {
   const renderItems = () => {
     return projectImage.map((renderBG, index) => (
       <div 
       key={index} 
-      className='projectSlidePassive flex-container-columns center-content'
+      className='projectSlidePassive'
       style={{ 
         backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)), url(${renderBG})`,
         backgroundSize: 'cover',
@@ -15,11 +17,16 @@ function PortfolioMain() {
         zIndex: '4'
       }}
       >
-          <h2 className='projectText'>{projectTitle[index]}</h2>
-          <div className='flexPortfolioWindow'>
-            <p className='projectText'>{projectText[index]}</p>
+          <div className='projectContainer'>
+          <div className='projectBox'>
+          <h2>{projectTitle[index]}</h2>
+          <p>{projectText[index]}</p>
           </div>
-
+          <div className="repoBox">
+            <a href={projectRepoLink[index]} target="_blank"><img className='deployandrepo' src={repoIcon} alt="Link to repo"/></a>
+            <a href={projectDeployLink[index]} target="_blank"><img className='deployandrepo' src={deployIcon} alt="Link to Deployed project" /></a>
+          </div>
+        </div>
       </div>
     ));
   };
