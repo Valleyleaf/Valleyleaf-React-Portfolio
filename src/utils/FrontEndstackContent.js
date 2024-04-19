13313e61590d46c6e24987c56f1f10dd0aa34f31
Aston .A Dahllof <137734906+Valleyleaf@users.noreply.github.com>
2024-04-19 11:59:49 -0300

    Turns out, storing my image assets on discord was a bad idea. Fixing paths and hosting

--- src/utils/FrontEndstackContent.js
+++ src/utils/FrontEndstackContent.js
@@ -1,42 +1,42 @@
 const FrontEndSkillData = [
     {
         _id: '1',
-        icon: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225566800310435870/html-5-svgrepo-com.svg?ex=66219907&is=660f2407&hm=2a545bef1ecb228dc5b05e4f3709b3648589fab09bc64649165635b4fd167cff&',
+        icon: 'https://www.svgrepo.com/show/452228/html-5.svg',
         title: 'HTML',
         stackSkillLevel: '50',
         alt: 'HTMLicon'
     },
     {
         _id: '2',
-        icon: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225566223463485501/css-3-svgrepo-com.svg?ex=6621987d&is=660f237d&hm=9338e7c392f5637e9ab8803909ae492a85b55f9615146ec0f8eb620cf5f2c682&',
+        icon: 'https://www.svgrepo.com/show/452185/css-3.svg',
         title: 'CSS',
         stackSkillLevel: '50',
         alt: 'CSSicon'
     },
     {
         _id: '3',
-        icon: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225566801052831935/js-svgrepo-com.svg?ex=66219907&is=660f2407&hm=843742d1873787ff24c7d64b2129ae7de1e3af85626515dda7bcd7edfb251f7a&',
+        icon: 'https://www.svgrepo.com/show/452045/js.svg',
         title: 'Javascript',
         stackSkillLevel: '50',
         alt: 'Javascripticon'
     },
     {
         _id: '4',
-        icon: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225566817079136366/python-svgrepo-com.svg?ex=6621990b&is=660f240b&hm=5a2488f810c814458bb42904c54bb5bf83492979672b6db67fbd85cbd5013933&',
+        icon: 'https://www.svgrepo.com/show/452091/python.svg',
         title: 'Python',
         stackSkillLevel: '50',
         alt: 'PythonIcon'
     },
     {
         _id: '5',
-        icon: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225566800717021285/java-svgrepo-com.svg?ex=66219907&is=660f2407&hm=c2ad412f77751c8804b7ee635f8f4cf91def0ddf13e0999e2bdb2098c16d7e48&',
+        icon: 'https://www.svgrepo.com/show/452234/java.svg',
         title: 'Java',
         stackSkillLevel: '50',
         alt: 'JavaIcon'
     },
     {
         _id: '6',
-        icon: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225566222850981958/csharp-svgrepo-com.svg?ex=6621987d&is=660f237d&hm=ceb2a7bd761293e8b6479cd22573230ba0992cf114fd348a8034f2a1c009a67b&',
+        icon: 'https://www.svgrepo.com/show/452184/csharp.svg',
         title: 'C#',
         stackSkillLevel: '50',
         alt: 'CSharpIcon'
--- src/utils/RepoprojectContent.js
+++ src/utils/RepoprojectContent.js
@@ -2,7 +2,7 @@ const projectData = [
     {
         _id: '1',
         title: 'moodMusic.com',
-        image: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225150355495452814/moodMusicExample.png?ex=6620152e&is=660da02e&hm=79f8d569e5642b5b7d252f96784bd142b54583cc37772a110995648cadfb9012&',
+        image: 'https://i.imgur.com/4c0UJ7w.png',
         textContent: 'Worked with a team of 4 to deploy moodMusic.com. A browser based application that allows you to generate a spotify playlist based on your location. This application runs off two seperate API\'s and was built using HTML, CSS and JavaScript.',
         repoLink: 'https://github.com/MagicianJoshua/weatherMusic',
         deployLink: 'https://magicianjoshua.github.io/weatherMusic/',
@@ -10,7 +10,7 @@ const projectData = [
     {
         _id: '2',
         title: 'Javascript Password Generator',
-        image: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225150354744807505/javapswgenerator.png?ex=6620152e&is=660da02e&hm=211883999033edc287ecc63a7791620dddbd2ef78c77ff22a00f1801e41f3655&',
+        image: 'https://i.imgur.com/661Silw.png',
         textContent: 'A javascript password generator which allows the user to create a password to suit their needs.',
         repoLink: 'https://github.com/Valleyleaf/Javascript-Password_generator',
         deployLink: 'https://valleyleaf.github.io/Javascript-Password_generator/',
@@ -18,7 +18,7 @@ const projectData = [
     {
         _id: '3',
         title: 'FOUR CATERING',
-        image: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225150354522505306/FourCatering.jpg?ex=6620152e&is=660da02e&hm=4db0bc377009a03a7a388d7dfe58e0070709e9742f38fb76f8e890ba96583d07&',
+        image: 'https://i.imgur.com/5OjJyi1.jpeg',
         textContent: 'Worked with a team of 4 in deployed a catering storefront using Heroku. Application supported on both front and back-end',
         repoLink: 'https://github.com/Valleyleaf/Catering-Application',
         deployLink: 'https://four-catering-cb97eed1384a.herokuapp.com/',
@@ -26,7 +26,7 @@ const projectData = [
     {
         _id: '4',
         title: 'Note-Taker',
-        image: 'https://cdn.discordapp.com/attachments/1058395094119620608/1225150352723148821/NoteTaker.png?ex=6620152e&is=660da02e&hm=a1b4a1f4b7e7e1f8c885e5ed7a30f1e9ed7ef547b404df8f0e6f2c7f25124659&',
+        image: 'https://i.imgur.com/Y8aPH5I.png',
         textContent: 'Deployed a Note-taker app that uses local storage to allow the user to host notes in their browser cache.',
         repoLink: 'https://github.com/Valleyleaf/Note-Taker',
         deployLink: 'https://valleyleaf.github.io/Note-Taker/',
@@ -34,7 +34,7 @@ const projectData = [
     {
         _id: '5',
         title: 'mySQL E-commerce Database',
-        image: 'https://cdn.discordapp.com/attachments/1058395094119620608/1227422681326944336/image.png?ex=66285973&is=6615e473&hm=d548414019a2e384c0039e2b0973dfddf188fd54d58c9f6740ef152f253f1424&',
+        image: 'https://i.imgur.com/g4sA83b.png',
         textContent: 'Structured database through mySQL for a E-commerce. For learning purposes only.',
         repoLink: 'https://github.com/Valleyleaf/Object-Relational-Mapping',
         deployLink: 'https://github.com/Valleyleaf/Object-Relational-Mapping',
@@ -42,7 +42,7 @@ const projectData = [
     {
         _id: '6',
         title: 'Kaizen-Esque',
-        image: 'https://cdn.discordapp.com/attachments/1058395094119620608/1227423855564423241/image.png?ex=66285a8b&is=6615e58b&hm=85502c3ae78e0ea408b196daff78c7e324e015de3f2cf3bf673b8af5a278aed8&',
+        image: 'https://i.imgur.com/QnYtAFf.png',
         textContent: 'Worked with a team of 5 to deploy a eLearning platform "Kaizen-Esque" where users can create coding tutorials and partake in learning.',
         repoLink: 'https://github.com/XenodiusAlpha/kaizen_esque',
         deployLink: 'https://kaizen-esque.onrender.com/',
