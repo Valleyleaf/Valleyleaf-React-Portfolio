fed5b05e1997de94c8a6c7f8b51cfbe3b90b0e92
Valleyleaf <aston_dahllof@outlook.com>
2025-11-25 09:53:02 -0400

    Messing around with styles

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,18 +1,24 @@
 import profilePicture from '../../assets/img/ProfilePicture.jpg'
 import './aboutme.css'
 import aboutMeJson from './aboutMe.json'
+import BannerImage from '../../assets/img/Banner.png'
 
 
   export default function About(){
   return (
     <> 
         <section id='aboutMeRef' className='AboutMeContainer'>
+          <div
+            className='aboutMeBackground'
+            style={{ backgroundImage: `url(${BannerImage})` }}
+          >
             <div className='aboutMeTextContainer fade-in'>
               <h2 className='aboutMetitle'>{aboutMeJson.aboutme.header}</h2>
               <p>{aboutMeJson.aboutme.description}</p>
               {/* <button>Interested in working with me?</button> */}
             </div>
-          <img className='profile-img' src={profilePicture} alt="ProfileImage"/>
+            <img className='profile-img' src={profilePicture} alt="ProfileImage"/>
+          </div>
         </section>
     </>
   );
