7c2821e98939af4eab144eac5f5d48460acc2e80
Valleyleaf <aston_dahllof@outlook.com>
2026-06-15 15:14:42 -0300

    Fixing SolutionStack

--- src/components/SolutionStack/SolutionStack.jsx
+++ src/components/SolutionStack/SolutionStack.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import StackContent from '../../utils/StackContent'
-import './solutionstack.css'
+import StackContent from '../../utils/StackContent.json';
+import './solutionstack.css';
 
-function SolutionStack(){
-        return (
-          <section id='stackRef' className='solutionStackContainer'>
-            <h2 className='stackTitle'>Solution Stack</h2>
-          </section>
-        );
-      }
+function SolutionStack() {
 
+  return (
+    <section id='stackRef' className='solutionStackContainer'>
+      <h2 className='stackTitle'>What I use</h2>
+      {Array.isArray(StackContent) && StackContent.map((stack) => (
+        <div key={stack.category} className='solutionFlexRow'>
+          {Array.isArray(stack.items) && stack.items.map((item) => (
+            <div key={item.id}>
+              <div className='stackContainer'>
+                <img className='stackImage' src={item.icon} alt={item.alt} />
+                <p>{item.title}</p>
+              </div>
+            </div>
+          ))}
+
+        </div>
+      ))}
+    </section>
+  );
+}
 
 export default SolutionStack;
