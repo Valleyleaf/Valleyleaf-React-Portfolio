abe639286bf0bcb2e15e3b8e8a95da2621163f94
Valleyleaf <aston_dahllof@outlook.com>
2026-06-29 16:04:24 -0300

    Adding a back to top button

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,8 @@ import './home.css'
 import SolutionStack from '../SolutionStack/SolutionStack.jsx'
 import AboutSection from '../AboutMe/AboutMe.jsx'
 import Projects from '../Projects/Projects.jsx'
+import BacktoTop from '../BacktoTopButton/BacktoTop.jsx'
+
 
 export default function Main(){
   return (
@@ -10,6 +12,7 @@ export default function Main(){
           <AboutSection/>
           <Projects/>
           <SolutionStack/>
+          <BacktoTop/>
       </main>
     </>
   );
